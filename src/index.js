cc02587b833e92cbe9b5782ca59adbb1b4db1aa5
Fabricio-Barreto <63824002+Fabricio-Barreto@users.noreply.github.com>
2022-04-10 19:59:57 -0300

    package.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const path = require("path")
 const taskRouter = require("../routes/TaskRoutes")
-const hbs = require('hbs')
 const connectToDb = require("../database/db")
 
 connectToDb()
