afcfe4202049c92267c2b9c74d0c588b62d0aa41
Fabricio-Barreto <63824002+Fabricio-Barreto@users.noreply.github.com>
2022-04-10 19:28:19 -0300

    router

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,12 +1,12 @@
 const router = require("express").Router()
 const TaskController = require("../controller/TaskController")
 
-router.get('/tasks', TaskController.getAllTask)
-router.post('/tasks', TaskController.createTask)
-router.get("/getById/:id", TaskController.getById)
-router.post("/updateOne/:id", TaskController.updateOneTask)
-router.get("/deleteOne/:id", TaskController.deleteOneTask)
-router.get("/check/:id", TaskController.taskCheck)
+router.get('/api/v1/tasks', TaskController.getAllTask)
+router.post('/api/v1/tasks', TaskController.createTask)
+router.get("/api/v1/getById/:id", TaskController.getById)
+router.post("/api/v1/updateOne/:id", TaskController.updateOneTask)
+router.get("/api/v1/deleteOne/:id", TaskController.deleteOneTask)
+router.get("/api/v1/check/:id", TaskController.taskCheck)
 
 
 module.exports = router
