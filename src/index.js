c6aaf9bd3bb3e79c3f43176cd72613d9392057fe
Fabricio-Barreto <63824002+Fabricio-Barreto@users.noreply.github.com>
2022-04-10 19:56:59 -0300

    add html.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require("path")
 const taskRouter = require("../routes/TaskRoutes")
+const hbs = require('hbs')
 const connectToDb = require("../database/db")
 
 connectToDb()
@@ -10,6 +11,17 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(taskRouter)
+
+const viewsPath = path.join(__dirname, '../templates/views')
+
+app.set("view engine", "ejs")
+app.set('views', viewsPath)
+
+app.get('/', (req, res) => {
+    res.render('index', {
+ 
+    })
+})     
 
 app.listen(port, () => {
     console.log(`Servidor rodando na Porta ${port}`)
