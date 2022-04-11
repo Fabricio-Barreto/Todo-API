7a91fbc7a042bf18b5eeb70055a144477df24e92
Fabricio-Barreto <63824002+Fabricio-Barreto@users.noreply.github.com>
2022-04-10 22:55:09 -0300

    Cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require("path")
 const routes = require("../routes/routes")
+const cors = require('cors')
 const connectToDb = require("../database/db")
 
 connectToDb()
@@ -10,6 +11,7 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(routes)
+app.use(cors())
 
 const viewsPath = path.join(__dirname, '../templates/views')
 
