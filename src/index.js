0264eafd93cd3ad7dab6e4a29b5e4bc4396a0c77
Fabricio-Barreto <63824002+Fabricio-Barreto@users.noreply.github.com>
2022-04-13 22:01:15 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(routes)
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
