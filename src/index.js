7e0bbe4028ee584c4035cfd06aa7bf9dbb14e6eb
Fabricio-Barreto <63824002+Fabricio-Barreto@users.noreply.github.com>
2022-04-13 21:03:44 -0300

    More cors error

--- src/index.js
+++ src/index.js
@@ -11,6 +11,13 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(routes)
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 const viewsPath = path.join(__dirname, '../templates/views')
 
