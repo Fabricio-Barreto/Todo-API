b2f425c35edce77e1708d52f48127b8e5083b198
Fabricio-Barreto <63824002+Fabricio-Barreto@users.noreply.github.com>
2022-04-13 22:20:28 -0300

    cors

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,11 @@ const cors = require ('cors')
 const TaskController = require("../controller/TaskController")
 
 router.get('/api/v1/tasks', cors (), TaskController.getAllTask)
-router.post('/api/v1/tasks', cors (), TaskController.createTask)
+router.post('/api/v1/tasks', cors ({
+    origin: '*',
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+
+}), TaskController.createTask)
 router.get("/api/v1/getById/:id", cors (), TaskController.getById)
 router.post("/api/v1/updateOne/:id", cors (), TaskController.updateOneTask)
 router.get("/api/v1/deleteOne/:id", cors (), TaskController.deleteOneTask)
